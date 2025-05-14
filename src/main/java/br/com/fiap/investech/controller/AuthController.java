713c30c24e2e8f48b3684ddc8116a5b77df69afa
Laboratório FIAP <labsfiap@fiap.com.br>
2025-05-14 08:48:05 -0300

    Correção de rotas

--- src/main/java/br/com/fiap/investech/controller/AuthController.java
+++ src/main/java/br/com/fiap/investech/controller/AuthController.java
@@ -2,10 +2,12 @@ package br.com.fiap.investech.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import br.com.fiap.investech.model.Credentials;
 import br.com.fiap.investech.model.User;
+import br.com.fiap.investech.repository.UserRepository;
 import br.com.fiap.investech.service.AuthService;
 import br.com.fiap.investech.service.TokenService;
 
@@ -19,6 +21,13 @@ public class AuthController {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    // Endpoint de login
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody Credentials credentials) {
         try {
@@ -28,5 +37,18 @@ public class AuthController {
         } catch (Exception e) {
             return ResponseEntity.status(401).body("Credenciais inválidas.");
         }
+    }
+
+    // Endpoint de registro
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        if (userRepository.findByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.badRequest().body("Usuário já cadastrado.");
+        }
+
+        // Criptografa a senha antes de salvar
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser);
     }
 }
--- src/main/java/br/com/fiap/investech/model/User.java
+++ src/main/java/br/com/fiap/investech/model/User.java
@@ -38,6 +38,10 @@ public class User implements UserDetails {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public String getUsername() {
         return email; // obrigatório para autenticação
@@ -46,6 +50,10 @@ public class User implements UserDetails {
     @Override
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     // Métodos obrigatórios de UserDetails
